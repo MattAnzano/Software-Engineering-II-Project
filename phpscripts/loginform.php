2a2806c0ea89b6ab57e255c6eff1bc9f356772f8
Matthew Anzano <m99anzano@gmail.com>
2022-02-25 13:22:35 -0500

    Reorganization of files

--- phpscripts/loginform.php
+++ phpscripts/loginform.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = "localhost:3306";
+$username = "root";
+$password = "test";
+$dbname = ""
 
 //placeholders
 $email = "email"
 $password = "password"
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysql_connect($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
