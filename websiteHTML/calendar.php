a26f5d0e7318cd13cfdba618c462f2407f22be62
OmaSmith <omarismithwork4@gmail.com>
2022-02-26 12:38:37 -0500

    converted some of the files to php. moved phpscripts into websiteHTML folder. converted the navbar + bootstrap scripts to individual files.

---
+++ websiteHTML/calendar.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+  <?php include 'phpscripts/navbar.php';?>
+  <?php echo 'hello world again!';?>
+  </body>
+</html>
