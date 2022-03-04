d16ec2d952165ac2d6ca5c50e5a343fa7dc4b789
Matthew Anzano <63332109+MattAnzano@users.noreply.github.com>
2022-03-04 13:42:13 -0500

    Create config.php
    This is the PHP configuration file that we will use to connect all of our PHP scripts to the database. This will allow our website to function and run SQL queries.

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'anzanom1_testuser');
+define('DB_PASSWORD', 'csit355-02');
+define('DB_NAME', 'anzanom1_golfBallRetail');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
