175612ea6df9e2ba6fd6fb9333f498b87753e983
Matthew Anzano <m99anzano@gmail.com>
2022-03-09 14:52:29 -0500

    last commit failed to go through

--- phpscripts/config.php
+++ phpscripts/config.php
@@ -14,7 +14,7 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }else{
     echo "Successfully connected to Database!";
-}
+} 
 ?>
 
 
--- phpscripts/loginform.php
+++ phpscripts/loginform.php
@@ -93,4 +93,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Close connection
     mysqli_close($link);
 }
-?>
+?> 
