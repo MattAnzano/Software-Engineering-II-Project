57cd04becbfc33b89aa41496450ccb591cbbac36
Matthew Anzano <m99anzano@gmail.com>
2022-03-10 16:46:26 -0500

    Changes made to hash the passwords as well as verify email address. (IF BROKEN REVERT TO CHANGE BEFORE THIS)

--- phpscripts/loginform.php
+++ phpscripts/loginform.php
@@ -36,7 +36,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password FROM Students WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
