1d39eed9a6b3567c9b1cc1bb558e7b1c3c545760
OmaSmith <omarismithwork4@gmail.com>
2022-04-03 12:49:47 -0400

    pointless commit, dw

--- newstuff/db.php
+++ newstuff/db.php
@@ -5,10 +5,10 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'manleya2');
 define('DB_PASSWORD', 'Welcome-2022');
 define('DB_NAME', 'manleya2_SoftwareEngineeringII');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
 if(!$link){
     die("ERROR: Could not connect. " . mysqli_connect_error());
