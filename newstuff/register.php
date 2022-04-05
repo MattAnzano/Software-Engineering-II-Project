d4edaa9b0fce987df25d4f1d0a5c06f93829d627
OmaSmith <omarismithwork4@gmail.com>
2022-04-04 20:24:21 -0400

    .

--- newstuff/register.php
+++ newstuff/register.php
@@ -82,10 +82,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && (empty($fname_err) && empty($lname_err)){
-      
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($fname_err) && empty($lname_err)){
+
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)"
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
