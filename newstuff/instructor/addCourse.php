cff8f15aea50c6bffe00f6b0df0014fcb17b4531
Matthew Anzano <m99anzano@gmail.com>
2022-04-20 20:42:55 -0400

    removed excess code as well as added some stuff to the addCourse and addStudent and remove student

--- newstuff/instructor/addCourse.php
+++ newstuff/instructor/addCourse.php
@@ -46,10 +46,41 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">SmartPlan</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html">Dashboard</a></li>
+          <!--<li><a href="about.html">About</a></li>-->
+          <li><a class="active" href="courses.html">Courses</a></li>
+          <!--<li><a href="trainers.html">Trainers</a></li>>-->
+          <!--<li><a href="events.html">Events</a></li>-->
+          <!--<li><a href="pricing.html">Pricing</a></li>-->
+
+         
+          <!--<li><a href="contact.html">Contact</a></li>-->
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="addCourse.php" class="get-started-btn">Add Course</a>
+
+    </div>
+  </header><!-- End Header -->
 </head>
 <body>
 
     <form action="addCourse.php" method="POST">
+      <br>
+      <br>
+      <br>
+      <br>
     <h1>Please Fill in the Course Information:</h1>
     <form>
   <!-- Course Name input -->
